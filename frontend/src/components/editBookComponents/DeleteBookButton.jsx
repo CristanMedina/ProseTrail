aff31d9d4f4ebdac585b3f9e6206fcfba462aa91
Cristan Medina <crishighlights@gmail.com>
2024-12-23 22:12:50 -0800

    [Actualizado] Eliminar libro ahora abre un modal donde te pregunta si estas seguro

--- frontend/src/components/editBookComponents/DeleteBookButton.jsx
+++ frontend/src/components/editBookComponents/DeleteBookButton.jsx
@@ -3,11 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { useWriteStore } from '../../store/writeStore';
 import { useAuthStore } from '../../store/authStore';
 import toast from 'react-hot-toast';
+import Modal from '../Modal';
+import { useState } from 'react';
 
 const DeleteBookButton = ({ book }) => {
     const { deleteBook, isLoading } = useWriteStore();
     const { user } = useAuthStore();
     const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleDelete = async () => {
         if (!book || !book._id) {
@@ -18,10 +21,11 @@ const DeleteBookButton = ({ book }) => {
         try {
             await deleteBook(book._id);
             toast.success("Libro eliminado con éxito.");
+            navigate(`/mis-libros/${user?._id}`);
         } catch (error) {
             console.error('Error details:', error);
         } finally {
-            navigate(`/mis-libros/${user?._id}`);
+            setIsModalOpen(false);
         }
     };
 
@@ -29,13 +33,32 @@ const DeleteBookButton = ({ book }) => {
         <div>
             <motion.button
                 className="mb-2 p-3 rounded-3xl font-semibold text-rose-700 bg-rose-200 hover:bg-rose-400 hover:text-rose-50 transition duration-200"
-                onClick={handleDelete}
+                onClick={() => setIsModalOpen(true)}
                 whileTap={{ scale: 0.95 }}
                 whileHover={{ scale: 1.05 }}
                 transition={{ type: "spring", stiffness: 400, damping: 17 }}
             >
-               {isLoading ? 'Eliminando...' : 'Eliminar'}
+                Eliminar
             </motion.button>
+
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <h2 className="text-lg font-bold">¿Estás seguro?</h2>
+                <p className="text-gray-600">El documento sera eliminado de forma permanente</p>
+                <div className="flex justify-end space-x-4 mt-4">
+                    <button
+                        className="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400"
+                        onClick={() => setIsModalOpen(false)}
+                    >
+                        Regresar
+                    </button>
+                    <button
+                        className="px-4 py-2 rounded bg-rose-500 text-white hover:bg-rose-600"
+                        onClick={handleDelete}
+                    >
+                        {isLoading ? 'Eliminando...' : 'Eliminar'}
+                    </button>
+                </div>
+            </Modal>
         </div>
     );
 };
